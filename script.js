c12d727691802b41f137ad1ee8d87a244767a8eb
tycompton <tyrone.compton@gmail.com>
2021-03-02 22:40:01 +0000

    navlink clicks now close menu

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Navbar
 const menu = document.querySelector('.menu')
 const navbar = document.querySelector('.navbar')
+const navlist = document.querySelector('.nav-list')
 
 menu.addEventListener('click', () => {
   navbar.classList.toggle('change')
   menu.classList.toggle('change')
 })
+
+navlist.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+  }
+)
